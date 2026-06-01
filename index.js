7585b6a06de436c84cad77ee34020136df5c75bd
ahsanshoib <ahsanshoib60@gmail.com>
2026-06-01 07:37:00 +0600

    manual cors headers

--- index.js
+++ index.js
@@ -31,6 +31,20 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization,Cookie");
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  next();
+});
+
 const client = new MongoClient(process.env.MONGODB_URI);
 let db;
 let auth;
